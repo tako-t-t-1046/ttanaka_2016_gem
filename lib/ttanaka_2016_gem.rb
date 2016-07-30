22c681454b2f48f4e6000d6db758212f55b771ab
None <tako.t.t.1046@gmail.com>
2016-07-30 08:39:02 +0000

    add newFunction

--- lib/ttanaka_2016_gem.rb
+++ lib/ttanaka_2016_gem.rb
@@ -1,5 +1,28 @@
 require "ttanaka_2016_gem/version"
 
 module Ttanaka2016Gem
-  # Your code goes here...
+  class Main 
+    # display "Hello"
+    def say_Hello
+      print 'Hello'
+    end
+
+    # calculate GCD
+    def culc_GCD(num1, num2)
+      if num1 < num2
+        temp = num1
+	num1 = num2
+	num2 = temp
+      end
+
+      while num2 > 0
+	r = num1 % num2
+	num1 = num2
+	num2 = r
+      end
+
+      return num1
+    end
+
+  end
 end
