7d84a62291d5cc42990b0f718d4860045b6e0389
Takashi Tanaka <takashi-tanaka@kccs.co.jp>
2016-08-09 18:55:34 +0900

    merged testcode

--- test/ttanaka_2016_gem_test.rb
+++ test/ttanaka_2016_gem_test.rb
@@ -19,5 +19,16 @@ class Ttanaka2016GemTest < Minitest::Test
     assert_equal true, @main.prime(11), '11 is prime'
     assert_equal false, @main.prime(10036), '10036 is not prime'
     assert_equal true, @main.prime(10037), '10037 is prime'
+
+  def test_GCD
+    assert_equal(15,@Main.calc_GCD(30, 45), 'GCD_1')
+    assert_equal(20,@Main.calc_GCD(20, 40), 'GCD_2')
+    assert_equal( 1,@Main.calc_GCD(23, 17), 'GCD_3')
+  end
+
+  def test_LCM
+    assert_equal( 12,@Main.calc_LCM( 3,  6), 'LCM_1')
+    assert_equal( 40,@Main.calc_LCM(20, 40), 'LCM_2')
+    assert_equal(391,@Main.calc_LCM(23, 17), 'LCM_3')
   end
 end
