c9259713c048d9efb108f428d339492e11185b39
tako-t-t-1046 <tako.t.t.1046@gmail.com>
2016-08-09 18:33:45 +0900

    Update test code for GCD, LCM

--- test/ttanaka_2016_gem_test.rb
+++ test/ttanaka_2016_gem_test.rb
@@ -5,7 +5,15 @@ class Ttanaka2016GemTest < Minitest::Test
     refute_nil ::Ttanaka2016Gem::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_GCD
+    assert_equal(15,@Main.calc_GCD(30, 45), 'GCD_1')
+    assert_equal(20,@Main.calc_GCD(20, 40), 'GCD_2')
+    assert_equal( 1,@Main.calc_GCD(23, 17), 'GCD_3')
   end
+  def test_LCM
+    assert_equal( 12,@Main.calc_LCM( 3,  6), 'LCM_1')
+    assert_equal( 40,@Main.calc_LCM(20, 40), 'LCM_2')
+    assert_equal(391,@Main.calc_LCM(23, 17), 'LCM_3')
+  end
+  
 end
